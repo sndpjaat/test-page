c373abd976c990f9536db20b033c58c2ae3eb0c3
Apple <sndpjaat008@gmail.com>
2024-05-23 18:08:05 +0530

    add padding

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,40 +19,43 @@ document.addEventListener("DOMContentLoaded", function () {
   
   // sldr
 
-  $('.act_item ').slick({
-    dots: true,
+
+  $('.responsive_2').slick({
+    dots: false,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    autoplay: true,
+    infinite: true,
+    centerMode: true,
+    variableWidth: true,
+    arrows: false,
+    autoplay: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1440,
+        breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
-          dots: true
+  
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 1,
+          slidesToScroll: 1,
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+  
     ]
   });
-    
+  
