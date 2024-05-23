52031ca9e56d7c223d8c8d5dffa45a2f19f264e9
Apple <sndpjaat008@gmail.com>
2024-05-23 23:11:33 +0530

    set slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,60 @@
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
-    document.querySelector(".menu").classList.toggle("cross")
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
 }
 
 
 // preloader
 document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("load", function () {
-      setTimeout(function () {
-        let loadingOverlay = document.getElementById("loadingOverlay");
-        loadingOverlay.style.display = "none";
-        document.querySelector("body").classList.remove("overflow_hidden");
-      }, 3000);
-    });
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      let loadingOverlay = document.getElementById("loadingOverlay");
+      loadingOverlay.style.display = "none";
+      document.querySelector("body").classList.remove("overflow_hidden");
+    }, 3000);
   });
-
-  
-  // sldr
+});
 
 
-  $('.responsive_2').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    infinite: true,
-    centerMode: true,
-    variableWidth: true,
-    arrows: false,
-    autoplay: false,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-  
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
+// sldr
+
+
+$('.scnd_sldr').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  infinite: true,
+  centerMode: true,
+  variableWidth: true,
+  arrows: false,
+  autoplay: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+
       }
-  
-    ]
-  });
-  
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+
+  ]
+});
